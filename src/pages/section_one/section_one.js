942b75ab2a1e9eaecfe27b819aee3ff81af3b8d6
PierreSymonPeralta <pierresymonperalta@gmail.com>
2018-02-20 21:43:21 +0800

    add background

--- src/pages/section_one/section_one.js
+++ src/pages/section_one/section_one.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './style.css';
 
 class SectionOne extends React.Component {
 	render() {
 		return (
 			<div className="section active" id="section1">
-      			<h1>Section One</h1>
+      			<h1>IvyIsHapPierreThanEver</h1>
     		</div>
 		)
 	}
