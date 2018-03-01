906da62cb49ae264489b3969e9c2428ba6c46e8e
PierreSymonPeralta <pierresymonperalta@gmail.com>
2018-03-01 20:46:37 +0800

    add slide components

--- src/pages/section_four/section_four.js
+++ src/pages/section_four/section_four.js
@@ -5,8 +5,35 @@ class SectionFour extends React.Component {
 	render() {
 		return (
 			<div className="section">
-      			<h1>Section Four</h1>
-    		</div>
+				<div className="slide active">
+      			        <div className="intro">
+      			          <h1>SectionTwo Page One</h1>
+      			          <p>Not only vertical scrolling but also horizontal scrolling. With fullPage.js you will be able to add horizontal sliders in the most simple way ever.
+      			          </p>
+      			        </div>
+      			 </div>
+      			 <div className="slide">
+      			         <div className="intro">
+      			           <h1>SectionTwo Page Two</h1>
+      			           <p>Not only vertical scrolling but also horizontal scrolling. With fullPage.js you will be able to add horizontal sliders in the most simple way ever.
+      			           </p>
+      			         </div>
+      			  </div>
+      			  <div className="slide">
+      			         <div className="intro">
+      			           <h1>SectionTwo Page Three</h1>
+      			           <p>Not only vertical scrolling but also horizontal scrolling. With fullPage.js you will be able to add horizontal sliders in the most simple way ever.
+      			           </p>
+      			         </div>
+      			  </div>
+      			  <div className="slide">
+      			          <div className="intro">
+      			            <h1>SectionTwo Page Four</h1>
+      			            <p>Not only vertical scrolling but also horizontal scrolling. With fullPage.js you will be able to add horizontal sliders in the most simple way ever.
+      			            </p>
+      			          </div>
+      			   </div>
+    	</div>
 		)
 	}
 }
