c17b4e4c90aae97b7509f2bed430ed3fd8dad534
PierreSymonPeralta <pierresymonperalta@gmail.com>
2018-05-07 21:33:01 +0800

    update pen card

--- src/components/PenCard.js
+++ src/components/PenCard.js
@@ -2,16 +2,13 @@ import React from 'react'
 import './style.css';
 
 const PenCard = (props) => {
-	console.log(window.location.origin + '/assets/img/' + props.src)
 	return (
 		<div className="pencard-container">
       <div className="image">
 				<img src={window.location.origin + '/assets/img/'+ props.src}alt="desc"/>	
-				<h1>{props.name}</h1>
-				<p>
-					{props.desc}
-				</p>
       </div>
+			<h1>{props.name}</h1>
+			<p>{props.desc}</p>
 		</div>
 	)
 }
